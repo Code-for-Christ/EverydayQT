a31406871aee0190042c4ceeecb0585be7b60071
nathankim0 <jinyeob07@gmail.com>
2022-02-05 18:43:31 +0900

    feat: parse json file

--- app/src/main/java/com/jinyeob/eqt/MainActivity.kt
+++ app/src/main/java/com/jinyeob/eqt/MainActivity.kt
@@ -2,10 +2,27 @@ package com.jinyeob.eqt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mccheynePlan: Plan
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        parseJson()
+    }
+
+    @ExperimentalSerializationApi
+    fun parseJson() {
+        val assetManager = this.resources.assets
+        val inputStream = assetManager.open("jsons/plan.json")
+        val jsonString = inputStream.bufferedReader().use { it.readText() }
+
+        mccheynePlan = Json.decodeFromString(jsonString)
+        mccheynePlan.plan.forEach { (key, value) -> println("$key = $value") }
     }
 }
