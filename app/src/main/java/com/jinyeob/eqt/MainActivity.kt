2d5a64a50d283be8268bc8cefbab9e377da090a6
nathankim0 <jinyeob07@gmail.com>
2022-03-30 10:11:03 +0900

    feat: 맥체인 json 가져오는 ViewModel 구현 (StateFlow)

--- app/src/main/java/com/jinyeob/eqt/MainActivity.kt
+++ app/src/main/java/com/jinyeob/eqt/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.jinyeob.eqt
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            viewModel.mccheyneFlow.collect {
+                if (it.plan.isNotEmpty()) {
+                    Log.d("tag", it.toString())
+                } else {
+                    Log.e("tag", "json 파싱 실패")
+                }
+            }
+        }
     }
 }
